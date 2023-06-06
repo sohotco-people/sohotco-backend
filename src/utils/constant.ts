dffa900822311968d07ccab64198209957cef7d4
devshon <wjdgns200297@gmail.com>
2023-06-06 22:12:40 +0900

    feat: constants

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -3,4 +3,3 @@ export const DATABASE_URL = process.env.DATABASE_URL as string
 export const KAKAO_REDIRECT_URI = process.env.KAKAO_REDIRECT_URI as string
 export const KAKAO_REST_API_KEY = process.env.KAKAO_REST_API_KEY as string
 export const SOHOTCO_OAUTH_KEY = process.env.SOHOTCO_OAUTH_KEY as string
-export const COOKEY_DOMAIN = process.env.COOKEY_DOMAIN as string
